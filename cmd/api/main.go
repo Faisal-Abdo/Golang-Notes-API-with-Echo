616bf26fab854f30ecf7b296b2b1714b9acab224
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2026-07-27 16:47:41 +0400

    Added Method error handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,6 +12,11 @@ func main() {
 }
 
 func getNotes(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	notes := []string{"Learn Go", "Build Notes API"}
 
 	w.Header().Set("Content-Type", "application/json")
