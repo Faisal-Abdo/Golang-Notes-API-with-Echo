cd379ad4aa95dbd987dcb7b437e24b9725a6f733
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2026-07-28 10:35:26 +0400

    Refactored notes handler to manage HTTP methods for notes operations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,9 +7,20 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/notes", getNotes)
+	http.HandleFunc("/notes", notesHandler)
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func notesHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getNotes(w, r)
+	case http.MethodPost:
+		createNote(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func getNotes(w http.ResponseWriter, r *http.Request) {
@@ -21,4 +32,22 @@ func getNotes(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	json.NewEncoder(w).Encode(notes.Notes)
+}
+
+func createNote(w http.ResponseWriter, r *http.Request) {
+	var note notes.Note
+
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	note.ID = len(notes.Notes) + 1
+	notes.Notes = append(notes.Notes, note)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(note)
 }
