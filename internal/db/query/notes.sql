67298e68b2964056400a5fe5864f330029821f51
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2026-08-02 09:38:11 +0400

    Add SQL queries for CRUD operations on notes for apis

--- internal/db/query/notes.sql
+++ internal/db/query/notes.sql
@@ -2,3 +2,22 @@
 SELECT id, title, content, created_at
 FROM notes
 ORDER BY id;
+
+-- name: GetNoteByID :one
+SELECT id, title, content, created_at
+FROM notes
+WHERE id = $1;
+
+-- name: CreateNote :one
+INSERT INTO notes (title, content)
+VALUES ($1, $2)
+RETURNING id, title, content, created_at;
+
+-- name: UpdateNoteByID :one
+UPDATE notes
+SET title = $1, content = $2
+WHERE id = $3
+RETURNING id, title, content, created_at;
+
+-- name: DeleteNoteByID :execrows
+DELETE FROM notes WHERE id = $1;
