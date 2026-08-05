92670afcf7f8f60205f847eebb8ed58892f3aef3
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2026-08-05 09:00:45 +0400

    Update database connection to use DATABASE_URL environment variable

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,14 +2,19 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func Open() (*sql.DB, error) {
 
-	connString := "postgres://postgres:postgres@localhost:5432/notesdb"
+	connString := os.Getenv("DATABASE_URL")
+	if connString == "" {
+		return nil, fmt.Errorf("DATABASE_URL environment variable is not set")
+	}
 	// Open a connection to the PostgreSQL database using the pgx driver
 	db, err := sql.Open("pgx", connString)
 	if err != nil {
